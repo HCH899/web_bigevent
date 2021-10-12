8a4b8ac3ee5e549222ecaed36de2900d49971ea5
HCH899 <9217694+hch899@user.noreply.gitee.com>
2021-10-12 22:33:47 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,21 @@
 $.ajaxPrefilter(function(options) {
-    console.log(options);
+    // console.log(options);
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-    console.log(options.url);
+        // console.log(options.url);
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 
+    options.complete = function(res) {
+        console.log('执行了complete回调');
+        // console.log(res);
+        console.log(res.responseJSON);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+
+    }
 })
